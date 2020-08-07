e8584c2f802f21382bc3ce9e10bcf9a6dca2492c
Guocongcong <2499292915@qq.com>
2020-08-07 11:06:01 +0800

    将函数对象的结果取反

---
+++ STL/容器适配器/函数适配器/取反器.cpp
@@ -0,0 +1,26 @@
+# include<iostream>
+# include<deque>
+# include<vector>
+# include<list>
+# include<algorithm>
+# include<iterator>
+# include<set>
+# include<map>
+# include<string>
+#include <functional>
+int main()
+{
+	int arr[]={5,3,23,54,7,9};
+	int len=sizeof(arr)/sizeof(arr[0]);
+	std::vector<int>vec(arr,arr+len);
+	std::vector<int>::iterator fit=std::find_if(vec.begin(),vec.end(),std::bind2nd(std::greater<int>(),10));
+	if(fit!=vec.end())
+	{
+		std::cout<<"vec find : "<<*fit<<std::endl;
+	}
+	else
+	{
+		std::cout<<"vec no exist"<<std::endl;
+	}
+}
+
